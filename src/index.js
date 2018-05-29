d9b85985ac34db1c1b7a02944054f536c1c4dc9d
Kaminski <kaminskij@b2b.regn.net>
2018-05-29 13:20:05 +0100

    Changes for module update

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './assets/index.css';
 import App from './components/App';
 import { WishList } from "./models/WishList";
 
-import {onSnapshot} from 'mobx-state-tree'
+import {onSnapshot, getSnapshot} from 'mobx-state-tree'
 
 let initialState = {
     items: [
@@ -14,25 +14,44 @@ let initialState = {
             image: "https://images-na.ssl-images-amazon.com/images/I/71CpQw%2BufNL._SL1000_.jpg"
         },
         {
-            name: "Miracles - C.S. Lewis",
+            name: "Miracles - C.S. Lewisfdsfds",
             price: 12.91,
             image:
                 "https://images-na.ssl-images-amazon.com/images/I/51a7xaMpneL._SX329_BO1,204,203,200_.jpg"
         }
     ]
 }
-if(localStorage.getItem("wishlistapp")){
-    const json  = initialState = JSON.parse(localStorage.getItem("wishlistapp"))
-    //check if model did not change
-    if(WishList.is(json))
-        initialState = json
-}
+// if(localStorage.getItem("wishlistapp")){
+//     const json  = initialState = JSON.parse(localStorage.getItem("wishlistapp"))
+//     //check if model did not change
+//     if(WishList.is(json))
+//         initialState = json
+// }
 
 
-const wishList = WishList.create(initialState)
+
+let wishList = WishList.create(initialState)
 
 onSnapshot(wishList, snapshot => {
     localStorage.setItem("wishlistapp", JSON.stringify(snapshot))
 })
 
-ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
+function renderApp(){
+    ReactDOM.render(<App wishList={wishList} />, document.getElementById('root'));
+}
+
+renderApp()
+
+if(module.hot){
+    module.hot.accept(['./components/App'], () => {
+        // NEW COMPONENTS
+        renderApp()
+    })
+
+    module.hot.accept(["./models/WishList"], ()=> {
+        //NEW MODEL DEFINITIONS
+        const snapshot = getSnapshot(wishList)
+        wishList = WishList.create(snapshot)
+        renderApp()
+    })
+}
